0086d8461d3ce238a49d0b4387b6d74f51737fe1
1mpl3m3nt3d <105541001+1mpl3m3nt3d@users.noreply.github.com>
2022-09-17 03:05:49 +0300

    UP

--- Catalog/Catalog.Host/Program.cs
+++ Catalog/Catalog.Host/Program.cs
@@ -77,41 +77,74 @@ builder.Services.AddCors(
         "CorsPolicy",
         builder => builder
             .SetIsOriginAllowed((host) => true)
-            .WithOrigins(configuration["SpaUrl"], configuration["PathBase"], configuration["Authorization:Authority"])
+            .WithOrigins(configuration["SpaUrl"], configuration["PathBase"], configuration["GlobalUrl"], configuration["Authorization:Authority"])
             .AllowAnyMethod()
             .AllowAnyHeader()
             .AllowCredentials()));
 
-if (configuration.GetValue<bool>("HEROKU_NGINX") == true)
+if (configuration["Nginx:UseNginx"] == "true" || Environment.GetEnvironmentVariable("Nginx__UseNginx") == "true")
 {
     try
     {
-        var socket = configuration["LinuxSocket"] ?? "/tmp/nginx.socket";
+        if (configuration["Nginx:UseInitFile"] == "true" || Environment.GetEnvironmentVariable("Nginx__UseInitFile") == "true")
+        {
+            var initFile = configuration["Nginx:InitFilePath"] ?? Environment.GetEnvironmentVariable("Nginx__InitFilePath") ?? "/tmp/app-initialized";
 
-        builder.WebHost.ConfigureKestrel((context, serverOptions) => serverOptions.ListenUnixSocket(socket));
+            if (!File.Exists(initFile))
+            {
+                File.Create(initFile).Close();
+            }
+
+            File.SetLastWriteTimeUtc(initFile, DateTime.UtcNow);
+        }
     }
     catch (Exception ex)
     {
-        Console.WriteLine($"Environment variable <HEROKU_NGINX> is set to <TRUE>, but there was an exception while configuring Kestrel for Listening Unix Socket:\n{ex.Message}");
+        Console.WriteLine($"Environment variable <Nginx__UseNginx> is set to 'true', but there was an exception while configuring Initialize File:\n{ex.Message}");
+    }
+
+    try
+    {
+        if (configuration["Nginx:UseUnixSocket"] == "true" || Environment.GetEnvironmentVariable("Nginx__UseUnixSocket") == "true")
+        {
+            var unixSocket = configuration["Nginx:UnixSocketPath"] ?? Environment.GetEnvironmentVariable("Nginx__UnixSocketPath") ?? "/tmp/nginx.socket";
+
+            builder.WebHost.ConfigureKestrel(kestrel => kestrel.ListenUnixSocket(unixSocket));
+        }
+        else
+        {
+            var portParsed = int.TryParse(configuration["PORT"] ?? Environment.GetEnvironmentVariable("PORT"), out var port);
+
+            if (portParsed)
+            {
+                builder.WebHost.ConfigureKestrel(kestrel => kestrel.ListenAnyIP(port));
+            }
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Environment variable <Nginx__UseNginx> is set to 'true', but there was an exception while configuring Kestrel:\n{ex.Message}");
     }
 }
 else
 {
-    if (Environment.GetEnvironmentVariable("PORT") != null)
-    {
-        try
-        {
-            var parsed = int.TryParse(Environment.GetEnvironmentVariable("PORT"), out var port);
+    var portEnv = configuration["PORT"] ?? Environment.GetEnvironmentVariable("PORT");
 
-            if (parsed)
+    try
+    {
+        if (portEnv != null)
+        {
+            var portParsed = int.TryParse(portEnv, out var port);
+
+            if (portParsed)
             {
-                builder.WebHost.ConfigureKestrel((context, serverOptions) => serverOptions.ListenAnyIP(port));
+                builder.WebHost.ConfigureKestrel(kestrel => kestrel.ListenAnyIP(port));
             }
         }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Environment variable <PORT> is set, but there was an exception while configuring Kestrel for Listening Port:\n{ex.Message}");
-        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Environment variable <PORT> is set to '{portEnv}', but there was an exception while configuring Kestrel:\n{ex.Message}");
     }
 }
 
@@ -139,25 +172,6 @@ app.UseEndpoints(endpoints =>
 });
 
 InitializeDB(app);
-
-if (configuration.GetValue<bool>("HEROKU_NGINX") == true)
-{
-    try
-    {
-        var initFile = configuration["InitializedFile"] ?? "/tmp/app-initialized";
-
-        if (!File.Exists(initFile))
-        {
-            File.Create(initFile).Close();
-        }
-
-        File.SetLastWriteTimeUtc(initFile, DateTime.UtcNow);
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine($"Environment variable <HEROKU_NGINX> is set to <TRUE>, but there was an exception:\n{ex.Message}");
-    }
-}
 
 app.Run();
 
